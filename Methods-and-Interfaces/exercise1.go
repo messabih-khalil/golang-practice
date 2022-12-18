e8d1e87ed1c0b3a131b00592c60b29a0b90a47ff
messabih-khalil <msbih.khalil@gmail.com>
2022-12-19 00:44:07 +0100

    interfaces and rcv function

---
+++ Methods-and-Interfaces/exercise1.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+// Create a new type called money. Its underlying type is float64.
+
+type money float64
+
+
+// Create a method called print that prints out the money value with exact 2 decimal points.
+
+func (m money) print(){
+	fmt.Printf("%.2f\n", m)
+}
+
+
+func main(){
+	moneys := []money{12.2545478 , 578.455 , 7895.78545}
+
+	for _ , val := range moneys{
+		val.print()
+	}
+}
