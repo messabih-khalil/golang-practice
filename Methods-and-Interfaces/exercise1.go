eb99bc134e80bd3e23ee0bf74d8732018e4c9ed3
messabih-khalil <msbih.khalil@gmail.com>
2022-12-19 00:55:09 +0100

    interfaces and rcv function exo1

--- Methods-and-Interfaces/exercise1.go
+++ Methods-and-Interfaces/exercise1.go
@@ -4,18 +4,18 @@ import "fmt"
 
 // Create a new type called money. Its underlying type is float64.
 
-type money float64
+type Money float64
 
 
 // Create a method called print that prints out the money value with exact 2 decimal points.
 
-func (m money) print(){
+func (m Money) print(){
 	fmt.Printf("%.2f\n", m)
 }
 
 
 func main(){
-	moneys := []money{12.2545478 , 578.455 , 7895.78545}
+	moneys := []Money{12.2545478 , 578.455 , 7895.78545}
 
 	for _ , val := range moneys{
 		val.print()
