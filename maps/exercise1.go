045b470d9145f9165396e304bfe003142e65c65c
messabih-khalil <msbih.khalil@gmail.com>
2022-12-18 23:22:43 +0100

    go maps

---
+++ maps/exercise1.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main(){
+	// 1. Declare a map called m1 which value is nil. Print out its type and value.
+	m1 := map[int]int{}
+	fmt.Printf("m1 type: %T, value: %#v\n", m1, m1)
+	// 2. Declare a map called m2. It's keys are of type int and values of type string.  Initialize the map using  a map literal with two key:value pairs.
+
+	var m2 map[int]string 
+}
